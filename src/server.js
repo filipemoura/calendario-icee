c197bb3259677b6d018215f6406e9dde27df91a1
Filipe Alves <filipemouraalves@gmail.com>
2021-05-26 11:04:09 -0300

    Alterando o server.js

--- src/server.js
+++ src/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./calendario-icee'));
+app.use(express.static('./dist/calendario-icee'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'calendario-icee/src/'}),
+    res.sendFile('index.html', {root: 'dist/calendario-icee/'}),
 );
 
 // Start the app by listening on the default Heroku port
